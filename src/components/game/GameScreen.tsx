5e7897649f063a58054aa226c98c000908250718
arlistunner-dot <arlistunner@gmail.com>
2026-06-03 00:14:01 +0500

    Update GameScreen.tsx

--- src/components/game/GameScreen.tsx
+++ src/components/game/GameScreen.tsx
@@ -83,7 +83,7 @@ const TOTAL_ROUNDS = 5;
 
 const SEAT_POSITIONS = [
   { top: '49%', left: '50%', transform: 'translate(-50%, -50%)' },
-  { top: '55%', left: '80%', transform: 'translate(-50%, -50%)' },
+  { top: '50%', left: '80%', transform: 'translate(-50%, -50%)' },
   { top: '60%', left: '90%', transform: 'translate(-50%, -50%)' },
   { top: '79%', left: '75%', transform: 'translate(-50%, -50%)' },
   { top: '79%', left: '19%', transform: 'translate(-50%, -50%)' },
